78f6e7173ab48cb710d63d40425bcaad31db5595
Jantz <jan.erik.jakstein@gmail.com>
2025-11-01 08:22:32 +0200

    Replace POST method with GET for login form, GitHub pages don't support POST.

--- src/loginForm.js
+++ src/loginForm.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('login-form');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', () => {
     const email = document.getElementById('email').value.trim();
     localStorage.setItem('isLoggedIn', 'true');
     localStorage.setItem('userEmail', email);
