7291a24b607af77473f3ee5a24ee64433b0eec66
Jantz <jan.erik.jakstein@gmail.com>
2025-11-01 08:18:24 +0200

    Test if preventDefault on form submission fixes 405 error on Github pages.

--- src/loginForm.js
+++ src/loginForm.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('login-form');
 
-form.addEventListener('submit', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const email = document.getElementById('email').value.trim();
     localStorage.setItem('isLoggedIn', 'true');
     localStorage.setItem('userEmail', email);
